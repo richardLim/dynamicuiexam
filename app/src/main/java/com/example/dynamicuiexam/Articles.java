048e241929bc5515e81ca4c0c3c246b954a7ebf9
richardlim <richardlim@shift.co.kr>
2021-01-15 14:57:29 +0900

    내용 텍스트 추가

--- app/src/main/java/com/example/dynamicuiexam/Articles.java
+++ app/src/main/java/com/example/dynamicuiexam/Articles.java
@@ -6,8 +6,8 @@ public class Articles {
     };
 
     static String[] Articles = {
-            "이것은 기사1의 내용입니다.",
-            "이것은 기사2의 내용입니다."
+            "이것은 기사1의 내용입니다!!",
+            "이것은 기사2의 내용입니다!!"
     };
 
 
